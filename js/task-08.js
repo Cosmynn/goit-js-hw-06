ff24a5e631f0ce2be2d3672ca2a66750d049df55
Cosmynn <142098825+Cosmynn@users.noreply.github.com>
2024-01-07 16:59:03 +0200

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const formFields = loginForm.elements;
+  const credentials = {};
+
+  for (let i = 0; i < formFields.length; i++) {
+    const element = formFields[i];
+    if (element.type !== 'submit') {
+      credentials[element.name] = element.value;
+    }
+  }
+
+  const allFieldsFilled = Object.values(credentials).every(value => value !== '');
+
+  if (allFieldsFilled) {
+    console.log(credentials);
+    loginForm.reset();
+  } else {
+    alert('All fields must be filled!');
+  }
+});
